04fbe83825141337d7cd8e7903d4a266455898e8
Mikaël Sévigny <sevignator@hotmail.com>
2025-06-11 10:37:24 -0400

    Add verbose mode

--- main.py
+++ main.py
@@ -2,26 +2,31 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+try:
+    user_prompt = sys.argv[1]
+except IndexError:
+    print("A prompt must be provided in order to generate a response.")
+    sys.exit(1)
 
 load_dotenv()
 
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 model = "gemini-2.0-flash-001"
-
-try:
-    prompt = sys.argv[1]
-except IndexError:
-    print("A prompt must be provided in order to generate a response.")
-    sys.exit(1)
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
 response = client.models.generate_content(
     model=model,
-    contents=prompt,
+    contents=messages,
 )
 
 print(response.text)
 
-if response.usage_metadata is not None:
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if "--verbose" in sys.argv:
+    print(f"User prompt: {user_prompt}")
+
+    if response.usage_metadata is not None and "--verbose" in sys.argv:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
